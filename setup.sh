2c515070badf968265fdb11b575588c9efd8ed53
Dinu <61915816+DinuNuggets@users.noreply.github.com>
2023-06-02 19:38:27 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,6 +18,9 @@ if [[ "$answer" == "n" || "$answer" == "N" ]]; then
     sudo apt upgrade
     sudo apt install curl
     sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+    export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
     source ~/.bashrc
     nvm install v18.15.0
     clear
