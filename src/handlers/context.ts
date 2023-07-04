7efe22e0be4a994faeafb14307faa6834987a929
Luis Otavio Martins <luisotaviomgr1@gmail.com>
2023-07-04 18:31:02 -0300

    1.0.2 version that fixes repetitive answers and added user context (#98)
    * fix: Stop Sydney from repeating previous answers
    
    * feat: Added easy way for users to set their own context
    
    * 1.0.2

--- src/handlers/context.ts
+++ src/handlers/context.ts
@@ -1,14 +1,19 @@
+import { oneLine, stripIndent } from "common-tags";
 import { Message } from "whatsapp-web.js";
 
 export async function getContext(message: Message) {
-  let context = "[system](#additional_instructions)\n";
+  let context = stripIndent`[system](#additional_instructions)
+  
+  # Important guidelines for your role as an assistant:
+  - You should not present yourself in every message.`;
+
   const contact = await message.getContact();
   const chat = await message.getChat();
 
   if (chat.isGroup)
-    context += `You are in a WhatsApp group chat. You don't know the group chat's name. This message was sent by: ${contact.pushname}.\nDon't use '@' to mention users when referring to them.`;
+    context += `- You are in a WhatsApp group chat. You don't know the group chat's name. This message was sent by: ${contact.pushname}.\nWhen referring to users, do not use '@' mentions.`;
   else
-    context += `You are in a WhatsApp private chat. The user name is ${contact.pushname}.`;
+    context += `- You are in a WhatsApp private chat. The user's name is ${contact.pushname}.`;
 
-  return context;
+  return context + process.env.USER_CONTEXT;
 }
