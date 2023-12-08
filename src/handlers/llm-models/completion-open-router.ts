d09b13ee25943b94960c65fb87005570ab2659cc
Luis Otavio Martins <luisotaviomgr1@gmail.com>
2023-12-08 00:15:55 -0300

    streaming respose, system message, context, etc (#234)

--- src/handlers/llm-models/completion-open-router.ts
+++ src/handlers/llm-models/completion-open-router.ts
@@ -1,7 +1,36 @@
+import { Message } from "whatsapp-web.js";
 import { chain } from "../../clients/open-router";
+import { STREAM_RESPONSES } from "../../constants";
 
-export async function getCompletionWithOpenRouter(messageText: string) {
-  let response = await chain.call({ input: messageText });
+export async function getCompletionWithOpenRouter(
+  message: Message,
+  context: string,
+  streamingReply: Message
+) {
+  let messageWithContext = context + message.body;
+  let tokenBuffer: string[] = ["..."];
+
+  let response = await chain.call(
+    { input: messageWithContext },
+    {
+      callbacks: [
+        {
+          async handleLLMNewToken(token: string) {
+            if (STREAM_RESPONSES !== "true") return;
+
+            // Buffer the token
+            tokenBuffer.push(token);
+
+            // Update streamingReply with buffered tokens
+            const updatedMessage = tokenBuffer.join("");
+
+            // Edit the streamingReply with the updated message
+            await streamingReply.edit(updatedMessage);
+          },
+        },
+      ],
+    }
+  );
 
   return response.text;
 }
