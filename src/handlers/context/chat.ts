e30935cda719886f41ae86e8009734fcc531e441
Luisotee <luisotaviomgr1@gmail.com>
2023-12-12 15:27:25 -0300

    feat: added group participants name into the context

--- src/handlers/context/chat.ts
+++ src/handlers/context/chat.ts
@@ -11,10 +11,21 @@ export async function getChatContext(message: Message) {
     const groupChat = chat as GroupChat;
     const contact = await whatsapp.getContactById(message.author as string);
     publicUserName = contact.pushname;
+    const groupContacts = await Promise.all(
+      groupChat.participants.map((participant) =>
+        whatsapp.getContactById(participant.id._serialized)
+      )
+    );
+    const groupContactNames = groupContacts
+      .map((contact) => contact.pushname)
+      .join(", ");
+
+    console.log("groupContactNames:", groupContactNames);
 
     chatContext = stripIndents`- You are in a group chat
     - There are ${groupChat.participants.length} participants in the group
     - The group's name is '${groupChat.name}'
+    - The group's participants are: ${groupContactNames}
     - '${publicUserName}' is who sent this message
   `;
   } else {
