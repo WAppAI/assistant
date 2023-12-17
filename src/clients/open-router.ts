9426b30a18e4ca6f66efb50cca42980f7f91a873
Luisotee <luisotaviomgr1@gmail.com>
2023-12-17 04:24:27 -0300

    Update dependencies and system message in open-router.ts

--- src/clients/open-router.ts
+++ src/clients/open-router.ts
@@ -1,4 +1,4 @@
-import { AgentExecutor, ChatAgentOutputParser } from "langchain/agents";
+import { AgentExecutor } from "langchain/agents";
 import { formatLogToString } from "langchain/agents/format_scratchpad/log";
 import { ReActSingleInputOutputParser } from "langchain/agents/react/output_parser";
 import { ChatOpenAI } from "langchain/chat_models/openai";
@@ -21,6 +21,7 @@ import {
   OPENROUTER_API_KEY,
   OPENROUTER_MEMORY_TYPE,
   OPENROUTER_MSG_MEMORY_LIMIT,
+  OPEN_ROUTER_SYSTEM_MESSAGE,
   SEARCH_API,
   SUMMARY_LLM_MODEL,
 } from "../constants";
@@ -123,44 +124,9 @@ export async function createExecutorForOpenRouter(
   const memory = await createMemoryForOpenRouter(chat);
 
   const systemMessageOpenRouter = PromptTemplate.fromTemplate(` 
-  Assistant is a large language model trained by OpenAI.
+${OPEN_ROUTER_SYSTEM_MESSAGE}
 
-Assistant is designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. As a language model, Assistant is able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
-
-Assistant is constantly learning and improving, and its capabilities are constantly evolving. It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, Assistant is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
-
-Overall, Assistant is a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether you need help with a specific question or just want to have a conversation about a particular topic, Assistant is here to assist.
-
-TOOLS:
-------
-
-Assistant has access to the following tools:
-
-{tools}
-
-To use a tool, please use the following format:
-
-\`\`\`
-Thought: Do I need to use a tool? Yes
-Action: the action to take, should be one of [{tool_names}]
-Action Input: the input to the action
-Observation: the result of the action
-\`\`\`
-
-When you have a response to say to the Human, or if you do not need to use a tool, you MUST use the format:
-
-\`\`\`
-Thought: Do I need to use a tool? No
-Final Answer: [your response here]
-\`\`\`
-
-Begin!
-
-Previous conversation history:
-{chat_history}
-
-New input: {input}
-{agent_scratchpad}`);
+${context}`);
 
   const promptWithInputs = await systemMessageOpenRouter.partial({
     tools: renderTextDescription(tools),
