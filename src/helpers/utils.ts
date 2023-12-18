bf5215709746a43d808c4c95f9592e041f834f08
Luis Otavio Martins <luisotaviomgr1@gmail.com>
2023-12-18 15:46:50 -0300

    Better .env.example, better environment variables check (#264)
    * Removed ENABLE_SEARCH_OPENROUTER
    
    * Update environment variables in .env.example

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,8 +1,3 @@
-import {
-  TRANSCRIPTION_METHOD,
-  OPENAI_API_KEY,
-  LOG_MESSAGES,
-} from "./../constants";
 import { Message } from "whatsapp-web.js";
 import { REACTIONS } from "../handlers/reactions";
 import { whatsapp } from "../clients/whatsapp";
@@ -11,8 +6,14 @@ import dayjs from "dayjs";
 export function checkEnv() {
   if (!process.env.OPENROUTER_API_KEY) {
     console.warn(
-      "OPENROUTER_API_KEY not provided. You must set a OPENROUTER_API_KEY. Please check your .env file."
+      "OPENROUTER_API_KEY not provided. You must set OPENROUTER_API_KEY. Please check your .env file."
     );
+  } else {
+    if (!process.env.SEARCH_API) {
+      console.warn(
+        "SEARCH_API not provided. You must set SEARCH_API. Please check your .env file."
+      );
+    }
   }
 
   if (!process.env.BING_COOKIES) {
@@ -23,200 +24,183 @@ export function checkEnv() {
 
   if (!process.env.BING_TONESTYLE) {
     console.warn(
-      "invalid BING_TONESTYLE provided. You must set a tonestyle. Please check your .env file."
+      "Invalid BING_TONESTYLE provided. You must set a tonestyle. Please check your .env file."
     );
   }
 
-  if (!process.env.BING_SYSTEM_MESSAGE)
-    throw new Error(
-      `Invalid BING_SYSTEM_MESSAGE="${process.env.BING_SYSTEM_MESSAGE}" provided. Please check the SYSTEM_MESSAGE variable your .env file.`
-    );
-
-  if (
-    !process.env.ENABLE_SUGGESTIONS ||
-    !["true", "false"].includes(process.env.ENABLE_SUGGESTIONS)
-  )
-    throw new Error(
-      `Invalid ENABLE_SUGGESTIONS="${process.env.ENABLE_SUGGESTIONS}" provided. Accepted values are "true" or "false". Please check the ENABLE_SUGGESTIONS variable your .env file.`
-    );
-
-  if (!process.env.OPENROUTER_MSG_MEMORY_LIMIT) {
-    throw new Error(
-      `Invalid OPENROUTER_MSG_MEMORY_LIMIT="${process.env.OPENROUTER_MSG_MEMORY_LIMIT}" provided. Please check the OPENROUTER_MSG_MEMORY_LIMIT variable your .env file.`
-    );
-  }
-  if (
-    !process.env.OPENROUTER_MEMORY_TYPE ||
-    !["buffer", "summary"].includes(process.env.OPENROUTER_MEMORY_TYPE)
-  ) {
-    throw new Error(
-      `Invalid OPENROUTER_MEMORY_TYPE="${process.env.OPENROUTER_MEMORY_TYPE}" provided. Please check the OPENROUTER_MEMORY_TYPE variable your .env file.`
-    );
-  }
-  if (process.env.OPENROUTER_MEMORY_TYPE === "summary") {
-    if (!process.env.SUMMARY_LLM_MODEL) {
-      throw new Error(
-        `Invalid SUMMARY_LLM_MODEL="${process.env.SUMMARY_LLM_MODEL}" provided. Please check the SUMMARY_LLM_MODEL variable your .env file.`
-      );
-    }
-    if (
-      !process.env.DEBUG_SUMMARY ||
-      !["true", "false"].includes(process.env.DEBUG_SUMMARY)
-    ) {
-      throw new Error(
-        `Invalid DEBUG_SUMMARY="${process.env.DEBUG_SUMMARY}" provided. Accepted values are "true" or "false". Please check the DEBUG_SUMMARY variable your .env file.`
-      );
-    }
-  }
-
-  if (!process.env.SEARCH_API) {
-    throw new Error(
-      `Invalid SEARCH_API="${process.env.SEARCH_API}" provided. Please check the SEARCH_API variable your .env file.`
-    );
-  }
-
-  if (process.env.BOT_PREFIX === process.env.CMD_PREFIX)
-    throw new Error(
-      `Invalid BOT_PREFIX/CMD_PREFIX provided. The bot prefix and the command prefix must be different. Please check your .env file.`
-    );
-  else if (!process.env.BOT_PREFIX && !process.env.CMD_PREFIX)
-    throw new Error(
-      `Invalid BOT_PREFIX/CMD_PREFIX provided. Both must not be empty. Please check your .env file.`
-    );
-
-  if (!process.env.ASSISTANT_NAME)
-    throw new Error(
-      `Invalid ASSISTANT_NAME="${process.env.ASSISTANT_NAME}" provided. Please check the ASSISTANT_NAME variable your .env file.`
-    );
-
-  if (!process.env.OPEN_ROUTER_SYSTEM_MESSAGE)
-    throw new Error(
-      `Invalid SYSTEM_MESSAGE="${process.env.SYSTEM_MESSAGE}" provided. Please check the SYSTEM_MESSAGE variable your .env file.`
-    );
-
-  if (!process.env.DATABASE_URL)
-    throw new Error(
-      `Invalid DATABASE_URL="${process.env.DATABASE_URL}" provided. Please check the DATABASE_URL variable your .env file.`
-    );
-
-  if (
-    !process.env.STREAM_RESPONSES ||
-    !["true", "false"].includes(process.env.STREAM_RESPONSES)
-  )
-    throw new Error(
-      `Invalid STREAM_RESPONSES="${process.env.STREAM_RESPONSES}" provided. Accepted values are "true" or "false". Please check the STREAM_RESPONSES variable your .env file.`
-    );
-
-  if (
-    !process.env.ENABLE_REMINDERS ||
-    !["true", "false"].includes(process.env.ENABLE_REMINDERS)
-  )
-    throw new Error(
-      `Invalid ENABLE_REMINDERS="${process.env.ENABLE_REMINDERS}" provided. Accepted values are "true" or "false". Please check the ENABLE_REMINDERS variable your .env file.`
-    );
-
-  if (
-    !process.env.STREAM_REMINDERS ||
-    !["true", "false"].includes(process.env.STREAM_REMINDERS)
-  )
-    throw new Error(
-      `Invalid STREAM_REMINDERS="${process.env.STREAM_REMINDERS}" provided. Accepted values are "true" or "false". Please check the STREAM_REMINDERS variable your .env file.`
-    );
-
-  if (
-    !process.env.REPLY_RRULES ||
-    !["true", "false"].includes(process.env.REPLY_RRULES)
-  )
-    throw new Error(
-      `Invalid REPLY_RRULES="${process.env.REPLY_RRULES}" provided. Accepted values are "true" or "false". Please check the REPLY_RRULES variable your .env file.`
-    );
-
-  if (
-    !process.env.ENABLE_SOURCES ||
-    !["true", "false"].includes(process.env.ENABLE_SOURCES)
-  )
-    throw new Error(
-      `Invalid ENABLE_SOURCES="${process.env.ENABLE_SOURCES}" provided. Accepted values are "true" or "false". Please check the ENABLE_SOURCES variable your .env file.`
-    );
-
-  if (
-    !process.env.ENABLE_REACTIONS ||
-    !["true", "dms_only", "groups_only", "false"].includes(
-      process.env.ENABLE_REACTIONS
-    )
-  )
-    throw new Error(
-      `Invalid ENABLE_REACTIONS="${process.env.ENABLE_REACTIONS}" provided. Accepted values are "true", "dms_only", "groups_only" or "false". Please check the ENABLE_REACTIONS variable your .env file.`
-    );
-
-  if (process.env.ENABLE_REACTIONS !== "false")
-    // Checks if all reactions are valid emojis
-    Object.values(REACTIONS).forEach((reaction) => {
-      if (!isEmoji(reaction)) {
-        throw new Error(
-          `Invalid reaction "${reaction}" provided. Please check the reactions variables your .env file. Make sure to only use emojis.`
-        );
-      }
-    });
-
-  if (process.env.BLOCKED_USERS)
+  if (!process.env.BING_SYSTEM_MESSAGE) {
     console.warn(
-      "BLOCKED_USERS provided. The bot will ignore messages from these users."
+      "Invalid BING_SYSTEM_MESSAGE provided. Please check the BING_SYSTEM_MESSAGE variable in your .env file."
     );
+  }
 
+  // Additional checks for optional variables related to audio transcription
+  // Check for audio transcription settings
   if (process.env.TRANSCRIPTION_ENABLED === "true") {
     if (
       !process.env.TRANSCRIPTION_METHOD ||
       !["whisper-api", "local"].includes(process.env.TRANSCRIPTION_METHOD)
-    )
+    ) {
       throw new Error(
-        `Invalid TRANSCRIPTION_METHOD="${process.env.TRANSCRIPTION_METHOD}" provided. Please check the TRANSCRIPTION_METHOD variable your .env file.`
+        `Invalid TRANSCRIPTION_METHOD="${process.env.TRANSCRIPTION_METHOD}" provided. Please check the TRANSCRIPTION_METHOD variable in your .env file.`
       );
-    if (process.env.TRANSCRIPTION_METHOD === "local") {
-      if (!process.env.TRANSCRIPTION_MODEL)
-        throw new Error(
-          `Invalid TRANSCRIPTION_MODEL="${process.env.TRANSCRIPTION_MODEL}" provided. Please check the TRANSCRIPTION_MODEL variable your .env file.`
-        );
     }
+
     if (
       !process.env.REPLY_TRANSCRIPTION ||
       !["true", "false"].includes(process.env.REPLY_TRANSCRIPTION)
-    )
+    ) {
       throw new Error(
-        `Invalid REPLY_TRANSCRIPTION="${process.env.REPLY_TRANSCRIPTION}" provided. Please check the REPLY_TRANSCRIPTION variable your .env file.`
+        `Invalid REPLY_TRANSCRIPTION="${process.env.REPLY_TRANSCRIPTION}" provided. Please check the REPLY_TRANSCRIPTION variable in your .env file.`
       );
-    if (!process.env.OPENAI_API_KEY || process.env.OPENAI_API_KEY === "")
-      throw new Error(
-        `Invalid OPENAI_API_KEY="${process.env.OPENAI_API_KEY}" provided. Please check the OPENAI_API_KEY variable your .env file.`
-      );
+    }
+
+    if (process.env.TRANSCRIPTION_METHOD === "local") {
+      if (!process.env.TRANSCRIPTION_MODEL) {
+        throw new Error(
+          `Invalid TRANSCRIPTION_MODEL="${process.env.TRANSCRIPTION_MODEL}" provided. Please check the TRANSCRIPTION_MODEL variable in your .env file.`
+        );
+      }
+    } else if (process.env.TRANSCRIPTION_METHOD === "whisper-api") {
+      if (!process.env.OPENAI_API_KEY || process.env.OPENAI_API_KEY === "") {
+        throw new Error(
+          `Invalid OPENAI_API_KEY="${process.env.OPENAI_API_KEY}" provided. Please check the OPENAI_API_KEY variable in your .env file.`
+        );
+      }
+    }
   }
+
+  // Additional checks for other optional variables
   if (
-    !process.env.LOG_MESSAGES ||
-    !["true", "false"].includes(process.env.LOG_MESSAGES)
-  )
+    process.env.STREAM_RESPONSES &&
+    !["true", "false"].includes(process.env.STREAM_RESPONSES)
+  ) {
     throw new Error(
-      `Invalid LOG_MESSAGES="${process.env.LOG_MESSAGES}" provided. Accepted values are "true" or "false". Please check the LOG_MESSAGES variable your .env file.`
+      `Invalid STREAM_RESPONSES="${process.env.STREAM_RESPONSES}" provided. Accepted values are "true" or "false". Please check the STREAM_RESPONSES variable in your .env file.`
     );
+  }
+
+  if (
+    process.env.ENABLE_REMINDERS &&
+    !["true", "false"].includes(process.env.ENABLE_REMINDERS)
+  ) {
+    throw new Error(
+      `Invalid ENABLE_REMINDERS="${process.env.ENABLE_REMINDERS}" provided. Accepted values are "true" or "false". Please check the ENABLE_REMINDERS variable in your .env file.`
+    );
+  }
+
+  if (
+    process.env.STREAM_REMINDERS &&
+    !["true", "false"].includes(process.env.STREAM_REMINDERS)
+  ) {
+    throw new Error(
+      `Invalid STREAM_REMINDERS="${process.env.STREAM_REMINDERS}" provided. Accepted values are "true" or "false". Please check the STREAM_REMINDERS variable in your .env file.`
+    );
+  }
+
+  if (
+    process.env.REPLY_RRULES &&
+    !["true", "false"].includes(process.env.REPLY_RRULES)
+  ) {
+    throw new Error(
+      `Invalid REPLY_RRULES="${process.env.REPLY_RRULES}" provided. Accepted values are "true" or "false". Please check the REPLY_RRULES variable in your .env file.`
+    );
+  }
+
+  if (
+    process.env.ENABLE_SOURCES &&
+    !["true", "false"].includes(process.env.ENABLE_SOURCES)
+  ) {
+    throw new Error(
+      `Invalid ENABLE_SOURCES="${process.env.ENABLE_SOURCES}" provided. Accepted values are "true" or "false". Please check the ENABLE_SOURCES variable in your .env file.`
+    );
+  }
+
+  // Check for reactions
+  if (
+    process.env.ENABLE_REACTIONS &&
+    !["true", "dms_only", "groups_only", "false"].includes(
+      process.env.ENABLE_REACTIONS
+    )
+  ) {
+    throw new Error(
+      `Invalid ENABLE_REACTIONS="${process.env.ENABLE_REACTIONS}" provided. Accepted values are "true", "dms_only", "groups_only" or "false". Please check the ENABLE_REACTIONS variable in your .env file.`
+    );
+  }
+
+  if (process.env.ENABLE_REACTIONS !== "false") {
+    // Checks if all reactions are valid emojis
+    Object.values(REACTIONS).forEach((reaction) => {
+      if (!isEmoji(reaction)) {
+        throw new Error(
+          `Invalid reaction "${reaction}" provided. Please check the reactions variables in your .env file. Make sure to only use emojis.`
+        );
+      }
+    });
+  }
+
+  if (process.env.BLOCKED_USERS) {
+    console.warn(
+      "BLOCKED_USERS provided. The bot will ignore messages from these users."
+    );
+  }
+
+  // Check other required variables
+  if (
+    !process.env.BOT_PREFIX ||
+    !process.env.CMD_PREFIX ||
+    process.env.BOT_PREFIX === process.env.CMD_PREFIX
+  ) {
+    throw new Error(
+      `Invalid BOT_PREFIX/CMD_PREFIX provided. The bot prefix and the command prefix must be different. Please check your .env file.`
+    );
+  }
+
+  if (!process.env.ASSISTANT_NAME) {
+    throw new Error(
+      `Invalid ASSISTANT_NAME="${process.env.ASSISTANT_NAME}" provided. Please check the ASSISTANT_NAME variable in your .env file.`
+    );
+  }
+
+  if (!process.env.OPEN_ROUTER_SYSTEM_MESSAGE) {
+    throw new Error(
+      `Invalid OPEN_ROUTER_SYSTEM_MESSAGE="${process.env.OPEN_ROUTER_SYSTEM_MESSAGE}" provided. Please check the OPEN_ROUTER_SYSTEM_MESSAGE variable in your .env file.`
+    );
+  }
+
+  if (!process.env.DATABASE_URL) {
+    throw new Error(
+      `Invalid DATABASE_URL="${process.env.DATABASE_URL}" provided. Please check the DATABASE_URL variable in your .env file.`
+    );
+  }
+
+  if (
+    process.env.LOG_MESSAGES &&
+    !["true", "false"].includes(process.env.LOG_MESSAGES)
+  ) {
+    throw new Error(
+      `Invalid LOG_MESSAGES="${process.env.LOG_MESSAGES}" provided. Accepted values are "true" or "false". Please check the LOG_MESSAGES variable in your .env file.`
+    );
+  }
 }
 
 export async function log(message: Message | null, isReply: boolean = false) {
-  if (LOG_MESSAGES === "false") return;
-  if (!message) return;
+  if (process.env.LOG_MESSAGES !== "false" && message) {
+    const chat = await message.getChat();
+    const contact = await message.getContact();
+    const chatName = chat.isGroup ? `@${chat.name}` : "@dm";
 
-  const chat = await message.getChat();
-  const contact = await message.getContact();
-  const chatName = chat.isGroup ? `@${chat.name}` : "@dm";
+    const from = contact.pushname;
+    const to =
+      chat.isGroup && isReply
+        ? (await (await message.getQuotedMessage()).getContact()).pushname
+        : (await whatsapp.getContactById(message.to)).pushname;
 
-  const from = contact.pushname;
-  const to =
-    chat.isGroup && isReply
-      ? (await (await message.getQuotedMessage()).getContact()).pushname
-      : (await whatsapp.getContactById(message.to)).pushname;
+    const timestamp = dayjs();
+    const timestampStr = timestamp.format("HH:mm:ss");
 
-  const timestamp = dayjs();
-  const timestampStr = timestamp.format("HH:mm:ss");
-
-  console.log(`${timestampStr} [${from}->${to}${chatName}]: ${message.body}`);
+    console.log(`${timestampStr} [${from}->${to}${chatName}]: ${message.body}`);
+  }
 }
 
 export function isEmoji(str: string) {
