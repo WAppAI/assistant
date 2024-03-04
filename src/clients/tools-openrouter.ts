0fe77ae5682eec8525050b87c081cd4920493217
Luis Otavio Martins <luisotaviomgr1@gmail.com>
2024-03-04 04:04:31 -0300

    feat: web browser tool (#279)

--- src/clients/tools-openrouter.ts
+++ src/clients/tools-openrouter.ts
@@ -1,7 +1,25 @@
 import { SearchApi } from "@langchain/community/tools/searchapi";
 import {
+  OPENROUTER_API_KEY,
   SEARCH_API
 } from "../constants";
+import { WebBrowser } from "langchain/tools/webbrowser";
+import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
+
+const OPENROUTER_BASE_URL = "https://openrouter.ai";
+
+const model = new ChatOpenAI(
+  {
+    modelName: "openai/gpt-3.5-turbo",
+    temperature: 0,
+    openAIApiKey: OPENROUTER_API_KEY,
+  },
+  {
+    basePath: `${OPENROUTER_BASE_URL}/api/v1`,
+  }
+);
+
+const embeddings = new OpenAIEmbeddings();
 
 let searchTool = null;
 if (SEARCH_API !== '') {
@@ -10,5 +28,7 @@ if (SEARCH_API !== '') {
   });
 }
 
-export const tools = searchTool ? [searchTool] : [];
+const webBrowserTool = new WebBrowser({ model, embeddings });
+
+export const tools = [...(searchTool ? [searchTool] : []), webBrowserTool];
 export const toolNames = tools.map((tool) => tool.name);
