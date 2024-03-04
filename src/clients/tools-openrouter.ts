5b0ab52f6f8a952df905b3f84576023913268220
Luis <luisotaviomgr1@gmail.com>
2024-03-04 03:41:32 -0300

    fix: searchTool can now be disabled

--- src/clients/tools-openrouter.ts
+++ src/clients/tools-openrouter.ts
@@ -1,50 +1,14 @@
 import { SearchApi } from "@langchain/community/tools/searchapi";
-import { OpenAI } from "langchain/llms/openai";
 import {
-  GoogleCalendarCreateTool,
-  GoogleCalendarViewTool,
-} from "langchain/tools/google_calendar";
-import {
-  GOOGLE_CALENDAR_CALENDAR_ID,
-  GOOGLE_CALENDAR_CLIENT_EMAIL,
-  GOOGLE_CALENDAR_PRIVATE_KEY,
-  OPENROUTER_API_KEY,
-  SEARCH_API,
+  SEARCH_API
 } from "../constants";
 
-const OPENROUTER_BASE_URL = "https://openrouter.ai";
+let searchTool = null;
+if (SEARCH_API !== '') {
+  searchTool = new SearchApi(SEARCH_API, {
+    engine: "google_news",
+  });
+}
 
-const model = new OpenAI(
-  {
-    modelName: "openai/gpt-3.5-turbo",
-    temperature: 0,
-    openAIApiKey: OPENROUTER_API_KEY,
-  },
-  {
-    basePath: `${OPENROUTER_BASE_URL}/api/v1`,
-  }
-);
-
-export const searchTool = new SearchApi(SEARCH_API, {
-  engine: "google_news",
-});
-
-const googleCalendarParams = {
-  credentials: {
-    clientEmail: GOOGLE_CALENDAR_CLIENT_EMAIL,
-    privateKey: GOOGLE_CALENDAR_PRIVATE_KEY,
-    calendarId: GOOGLE_CALENDAR_CALENDAR_ID,
-  },
-  scopes: [
-    "https://www.googleapis.com/auth/calendar",
-    "https://www.googleapis.com/auth/calendar.events",
-  ],
-  model,
-};
-
-const CalendarCreateTool = new GoogleCalendarCreateTool(googleCalendarParams);
-
-const CalendarViewTool = new GoogleCalendarViewTool(googleCalendarParams);
-
-export const tools = [searchTool, CalendarCreateTool, CalendarViewTool];
+export const tools = searchTool ? [searchTool] : [];
 export const toolNames = tools.map((tool) => tool.name);
