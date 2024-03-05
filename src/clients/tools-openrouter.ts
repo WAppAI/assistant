2f071eed896bd19355c57ff68903939045a983e7
Luis <luisotaviomgr1@gmail.com>
2024-03-05 04:54:25 -0300

    feat: Google Calendar tool working

--- src/clients/tools-openrouter.ts
+++ src/clients/tools-openrouter.ts
@@ -1,10 +1,15 @@
 import { SearchApi } from "@langchain/community/tools/searchapi";
 import {
+  OPENAI_API_KEY,
   OPENROUTER_API_KEY,
   SEARCH_API
 } from "../constants";
 import { WebBrowser } from "langchain/tools/webbrowser";
-import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
+import { ChatOpenAI, OpenAI, OpenAIEmbeddings } from "@langchain/openai";
+import {
+  GoogleCalendarCreateTool,
+  GoogleCalendarViewTool,
+} from "@langchain/community/tools/google_calendar";
 
 const OPENROUTER_BASE_URL = "https://openrouter.ai";
 
@@ -19,6 +24,24 @@ const model = new ChatOpenAI(
   }
 );
 
+const googleCalendarModel = new OpenAI({
+  temperature: 0,
+  openAIApiKey: OPENAI_API_KEY,
+});
+
+const googleCalendarParams = {
+  credentials: {
+    clientEmail: process.env.GOOGLE_CALENDAR_CLIENT_EMAIL,
+    privateKey: process.env.GOOGLE_CALENDAR_PRIVATE_KEY,
+    calendarId: process.env.GOOGLE_CALENDAR_CALENDAR_ID,
+  },
+  scopes: [
+    "https://www.googleapis.com/auth/calendar",
+    "https://www.googleapis.com/auth/calendar.events",
+  ],
+  model: googleCalendarModel,
+};
+
 const embeddings = new OpenAIEmbeddings();
 
 let searchTool = null;
@@ -28,7 +51,9 @@ if (SEARCH_API !== '') {
   });
 }
 
+const googleCalendarCreateTool = new GoogleCalendarCreateTool(googleCalendarParams)
+const googleCalendarViewTool = new GoogleCalendarViewTool(googleCalendarParams)
 const webBrowserTool = new WebBrowser({ model, embeddings });
 
-export const tools = [...(searchTool ? [searchTool] : []), webBrowserTool];
+export const tools = [...(searchTool ? [searchTool] : []), webBrowserTool, googleCalendarCreateTool, googleCalendarViewTool];
 export const toolNames = tools.map((tool) => tool.name);
